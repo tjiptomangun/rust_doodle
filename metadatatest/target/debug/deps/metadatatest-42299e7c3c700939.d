8a554012988e8e709b5818bd0d7f1fb8a8e40576
hanky <hanky.adhi.chandra@ansvia.com>
2019-02-18 20:12:59 +0700

    reqwest

--- metadatatest/target/debug/deps/metadatatest-42299e7c3c700939.d
+++
@@ -1,5 +0,0 @@
-/home/hanky/rust/notes/metadatatest/target/debug/deps/metadatatest-42299e7c3c700939: src/main.rs
-
-/home/hanky/rust/notes/metadatatest/target/debug/deps/metadatatest-42299e7c3c700939.d: src/main.rs
-
-src/main.rs:
--- metadatatest/target/debug/metadatatest.d
+++
@@ -1,1 +0,0 @@
-/home/hanky/rust/notes/metadatatest/target/debug/metadatatest: /home/hanky/rust/notes/metadatatest/src/main.rs
