5b2901ceb95f93be3673ef1e509a24baafd07176
tjiptomangun <tjiptomangun@yahoo.com>
2019-02-24 21:17:03 +0700

    lifetime

--- ownership/lifetime/mytest.rs
+++ ownership/lifetime/mytest.rs
@@ -1,21 +1,54 @@
-fn terong()-> &str {
-	instr
-}
+//does not work
+//fn terong()-> &str {
+//	 "terong" 
+//}
 
 fn jamur(instr: & str)-> &str {
 	instr
 }
 
+fn kol() -> &'static str {
+	"kol"
+	
+}
+
+fn sawi<'a>() -> &'a str {
+	"sawi"
+}
+
+fn bayam() -> String {
+	"bayam".to_string()
+}
+
+fn daunsingkong() -> String{
+	let ret:  String  = "daun singkong".to_string();
+	ret
+}
+
+fn gori(instr: String) -> String {
+	let a = "gori";
+	let b = format!("{} {}", a , instr);
+	let c = format!("{} {}", a , instr);
+	b
+}
+
+
 fn goreng(instr1: &str, instr2: &str) {
-	println!("terong  {} goreng {}", instr1, instr2);
+	println!("{} goreng {}", instr1, instr2);
 }
 
 fn main() {
-	let b = "belanda";
-	let c = "mentega";
+	let b = "jamur";
 
-	let trg = terong(b);
-	goreng(trg, c);
+	//let trg = terong();
 	let jmr = jamur(b);
-	goreng(jmr, c);
+	let kll = kol();
+	let c = sawi();
+	let d = sawi();
+	let bym = bayam();
+	let dns = daunsingkong();
+	let gri = gori("bakar".to_string());
+
+	println!("jmr {} kol {} sawi {} sawi {} ", jmr, kll, c, d);
+	println!("bym {} dns {} gri{} ", bym, dns, gri);
 }
